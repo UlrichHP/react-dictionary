ffde65b0fc840f5774934af2aace95c66fd806ee
Ulrich <ulrichhuet@hotmail.com>
2021-08-09 01:15:22 +0200

    feat(definitions): added definitions component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import axios from 'axios';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import './App.css';
+import Definitions from './components/Definitions/Definitions';
 import Header from './components/Header/Header';
 
 function App() {
-  const [data, setData] = useState([])
+  const [datas, setDatas] = useState([])
   const [category, setCategory] = useState('en')
   const [word, setWord] = useState('')
 
   const dictionaryApi = async() => {
     try {
       const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`)
-      setData(data.data)
+      setDatas(data.data)
     } catch (error) {
       console.log(error)
     }
   }
-
-  console.log(data)
 
   useEffect(() => {
     dictionaryApi()
@@ -29,6 +28,7 @@ function App() {
     <div className="App">
       <Container className="container" maxWidth="md">
         <Header category={category} setCategory={setCategory} word={word} setWord={setWord} />
+        {datas && (<Definitions word={word} datas={datas} category={category} />)}
       </Container>
     </div>
   );
